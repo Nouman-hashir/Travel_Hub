e68bf5d058663a6b5d49b43688dbc2d064423873
Code_with_nouman <134682051+Nouman-hashir@users.noreply.github.com>
2025-09-23 16:21:38 +0500

    Add onboarding screen and assets to project
    Introduced an onboarding screen with background and text images, updated main.dart to display the onboarding screen, and added a placeholder for the bottom navigation bar. Also updated pubspec.yaml to include image assets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_hub/presentation/view/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Travel App',
-      home: Container()
+      home: OnboardingScreen(),
     );
   }
 }
---
+++ lib/presentation/view/onboarding_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/on_boarding.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 60,
+            left: 80,
+            child: Image.asset('assets/images/aspen_text.png'),
+          ),
+          Positioned(
+            bottom: 100,
+            left: 30,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Plan Your',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w300,
+                    letterSpacing: 2,
+                  ),
+                ),
+                Text(
+                  'Luxurious',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 38,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: 4,
+                 
+                  ),
+                ),
+                Text(
+                  'Vacation',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: 2,
+                  ),
+                ),
+                SizedBox(height: 10),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            left: 36,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF196EEE),
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 140, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Explore',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 1,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
